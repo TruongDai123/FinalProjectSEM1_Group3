e8586c06fef950033e07c61dafa58cc8f16df342
NgoTuanVTCA <tuanna.nde18037@vtc.edu.vn>
2019-06-04 17:46:30 +0700

    update 1

---
+++ PL_Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Security;
+using BL;
+using System.Text.RegularExpressions;
+using Persistence;
+
+namespace PL_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menus m = new Menus();
+            m.MenuChoice(null);
+        }
+    }
+}
